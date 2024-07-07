a4af71c8242e8e08da73b050490bde8f7cbaf6f2
seerlog <selfandtogether@gmail.com>
2024-07-07 20:55:17 +0900

    add 01

---
+++ _01_entity_manager/src/main/java/com/example/_01_entity_manager/Application.java
@@ -0,0 +1,13 @@
+package com.example._01_entity_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ _01_entity_manager/src/test/java/com/example/_01_entity_manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._01_entity_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
